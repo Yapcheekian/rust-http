1f6fccdf002321128fed1c03316f22a9723fdec7
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-05-22 10:04:44 +0800

    add custom error

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,50 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FmtResult, Formatter, Debug, Display};
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,8 @@
 use std::net::TcpListener;
+use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     addr: String,
@@ -13,6 +17,27 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
-        let listener = TcpListener::bind(&self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(Request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e)
+                    }
+                }
+                Err(e) => println!("Failed to etablish connection: {}", e),
+            }
+        }
     }
 }
